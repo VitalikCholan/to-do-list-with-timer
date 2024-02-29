c2f25d4b1455a49d779133363ace88b9abc9a841
Віталій Чолан <vitalikcolan@gmail.com>
2024-02-29 13:08:39 +0200

    Added features in tasks section
    Added features in tasks section, where users can add or delete their tasks

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ const tasksBox = document.querySelector('.tasks-box');
 const tasksTitle = document.querySelector('.tasks-title');
 const tasksCount = document.querySelector('.tasks-count');
 const tasksNames = document.querySelector('.tasks-names');
+const taskTemplate = document.getElementById('task-template');
+const addTask = document.querySelector('.add-task');
+const newTask = document.querySelector('.new-task');
+const deleteTask = document.querySelector('.btn-delete-task');
 
 // Local storage
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
@@ -17,15 +21,31 @@ let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 listsNames.addEventListener('click', e => {
-    if (e.target.tagName.toLowerCase() === 'li'){
+    if (e.target.tagName.toLowerCase() === 'li') {
         selectedListId = e.target.dataset.listId;
         saveAndRender();
+    }
+})
+
+tasksNames.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'input') {
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
+        selectedTask.complete = e.target.checked;
+        save();
+        renderTaskCount(selectedList);
     }
 })
 
 deleteList.addEventListener('click', e => {
     lists = lists.filter(list => list.id !== selectedListId);
     selectedListId = null;
+    saveAndRender();
+})
+
+deleteTask.addEventListener('click', e => {
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
     saveAndRender();
 })
 
@@ -41,8 +61,25 @@ addList.addEventListener('submit', e => {
     saveAndRender();
 })
 
+addTask.addEventListener('submit', e => {
+    e.preventDefault();
+    const taskName = newTask.value;
+    if (taskName === null || taskName === '') {
+        return null;
+    }
+    const task = createTask(taskName);
+    newTask.value = null;
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks.push(task);
+    saveAndRender();
+})
+
 function createList(name) {
-    return { id: Date.now().toString(), name: name, task: [] };
+    return { id: Date.now().toString(), name: name, tasks: [] };
+}
+
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false };
 }
 
 function saveAndRender() {
@@ -57,12 +94,45 @@ function save() {
 
 function render() {
     clearElement(listsNames);
+    renderList();
+    const selectedList = lists.find(list => list.id === selectedListId);
+    if (selectedListId === null) {
+        tasksBox.style.display = 'none' ;
+    } else {
+        tasksBox.style.display = '';
+        tasksTitle.innerText = selectedList.name;
+        renderTaskCount(selectedList); 
+        clearElement(tasksNames);
+        renderTasks(selectedList);
+    }
+}
+
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true);
+        const checkbox = taskElement.querySelector('input');
+        checkbox.id = task.id;
+        checkbox.checked = task.complete;
+        const label = taskElement.querySelector('label');
+        label.htmlFor = task.id;
+        label.append(task.name);
+        tasksNames.appendChild(taskElement);
+    })
+}
+
+function renderTaskCount(selectedList) {
+    const incompleteTasksCount = selectedList.tasks.filter(task => !task.complete).length;
+    const taskString = incompleteTasksCount === 1 ? 'task' : 'tasks';
+    tasksCount.innerText = `${incompleteTasksCount} ${taskString} remaining`;
+}
+
+function renderList() {
     lists.forEach(list => {
         const listElement = document.createElement('li');
         listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
         listElement.innerText = list.name;
-        if (list.id === selectedListId){
+        if (list.id === selectedListId) {
             listElement.classList.add('active-list');
         }
         listsNames.appendChild(listElement);
@@ -70,7 +140,7 @@ function render() {
 }
 
 function clearElement(element) {
-    while(element.firstChild) {
+    while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
 }
