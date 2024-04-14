219c2c4e99d79b2928de0d43fa3af68f6b7f5971
Віталій Чолан <vitalikcolan@gmail.com>
2024-04-14 15:39:53 +0300

    added responsive typography and changed alert message

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import { todoList } from "./to-do-list.js";
-import { timerToDo } from "./timer.js";
-
-timerToDo();
-todoList();
--- js/timer.js
+++ js/timer.js
@@ -1,89 +1,88 @@
-export function timerToDo() {
+const activeTask = document.querySelector('.active-task');
+const timerNumbers = document.querySelector('.timer-numbers');
+const startStopBtn = document.querySelector('.startStopBtn');
+const resetBtn = document.querySelector('.resetBtn');
 
-    const activeTask = document.querySelector('.active-task');
-    const timerNumbers = document.querySelector('.timer-numbers');
-    const startStopBtn = document.querySelector('.startStopBtn');
-    const resetBtn = document.querySelector('.resetBtn');
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-    let seconds = 0;
-    let minutes = 0;
-    let hours = 0;
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
 
-    let leadingSeconds = 0;
-    let leadingMinutes = 0;
-    let leadingHours = 0;
+let timerInterval = null;
+let timerStatus = 'stopped';
 
-    let timerInterval = null;
-    let timerStatus = 'stopped';
+function timer() {
+  seconds++;
 
-    function timer() {
-        seconds++;
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
 
-        if(seconds / 60 === 1) {
-            seconds = 0;
-            minutes++;
-
-            if(minutes / 60 === 1) {
-                minutes = 0;
-                hours++;
-            }
-
-                if(hours / 24 === 1) {
-                    hours = 0;
-                }
-        }
-
-        seconds < 10 ? leadingSeconds = `0${seconds.toString()}` : leadingSeconds = seconds;
-
-        minutes < 10 ? leadingMinutes = `0${minutes.toString()}` : leadingMinutes = minutes;
-
-        hours < 10 ? leadingHours = `0${hours.toString()}` : leadingHours = hours;
-
-
-        timerNumbers.innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`;
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++;
     }
 
-    const alertBox = document.querySelector('.alert');
-    const closeBtn = document.querySelector('.close-btn');
+    if (hours / 24 === 1) {
+      hours = 0;
+    }
+  }
 
-    startStopBtn.addEventListener('click', () => {
-        if(timerStatus === 'stopped') {
-            if(activeTask.value.trim() === '') {
-                alertBox.classList.add('show');
-                alertBox.classList.remove('hide');
-                alertBox.classList.add('showAlert');
-                setTimeout(() => {
-                    alertBox.classList.remove('show');
-                    alertBox.classList.add('hide');
-                }, 5000);
-                return;        
-            } else {
-                alertBox.classList.remove('show');
-                alertBox.classList.add('hide');
-            }
+  seconds < 10
+    ? (leadingSeconds = `0${seconds.toString()}`)
+    : (leadingSeconds = seconds);
 
-            timerInterval = setInterval(timer, 1000);
-            startStopBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
-            timerStatus = 'started';
-        } else {
-            clearInterval(timerInterval);
-            startStopBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
-            timerStatus = 'stopped';
-        }
-    })
+  minutes < 10
+    ? (leadingMinutes = `0${minutes.toString()}`)
+    : (leadingMinutes = minutes);
 
-    resetBtn.addEventListener('click', () => {
-        clearInterval(timerInterval);
-        seconds = 0;
-        minutes = 0;
-        hours = 0;
+  hours < 10 ? (leadingHours = `0${hours.toString()}`) : (leadingHours = hours);
 
-        timerNumbers.innerText = '00:00:00';
-    })
+  timerNumbers.innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`;
+}
 
-    closeBtn.addEventListener('click', () => {
+const alertBox = document.querySelector('.alert');
+const closeBtn = document.querySelector('.close-btn');
+
+startStopBtn.addEventListener('click', () => {
+  if (timerStatus === 'stopped') {
+    if (activeTask.value.trim() === '') {
+      alertBox.classList.add('show');
+      alertBox.classList.remove('hide');
+      alertBox.classList.add('showAlert');
+      setTimeout(() => {
         alertBox.classList.remove('show');
         alertBox.classList.add('hide');
-    })
+      }, 5000);
+      return;
+    } else {
+      alertBox.classList.remove('show');
+      alertBox.classList.add('hide');
+    }
 
-}
+    timerInterval = setInterval(timer, 1000);
+    startStopBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    timerStatus = 'started';
+  } else {
+    clearInterval(timerInterval);
+    startStopBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+    timerStatus = 'stopped';
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+  clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  timerNumbers.innerText = '00:00:00';
+});
+
+closeBtn.addEventListener('click', () => {
+  alertBox.classList.remove('show');
+  alertBox.classList.add('hide');
+});
